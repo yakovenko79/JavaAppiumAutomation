d8ad3717b59e574bb2d9928b3ab1dff9a57321e4
MIKHAIL IAKOVENKO <114107429+yakovenko79@users.noreply.github.com>
2024-07-04 21:26:02 +0300

    feat: create setup and teardown fixtures

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,2 +1,34 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
 public class FirstTest {
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Androide");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("appium:automationName", "UiAutomator2");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "C:\\Users\\MIKA\\Desktop\\JavaAppiumAutomation\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), capabilities);
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void firstTest() {
+        System.out.println("first test run");
+    }
 }
